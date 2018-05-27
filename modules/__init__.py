d6f105871e2064944979094aa0ee250b8c159635
Jari Koopman <j.koopman@jarict.nl>
2018-05-27 15:48:14 +0200

    Start on search module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,4 @@
 from modules.module import Module
 from modules.create import CreateModule
 from modules.view import ViewModule
+from modules.search import SearchModule
---
+++ modules/search.py
@@ -0,0 +1,4 @@
+from modules import Module
+
+class SearchModule(Module):
+    pass
